d49975c81200aef073274d06e8166d11dbdb5aad
Benjamin Ruston <benjy.ruston@gmail.com>
2020-02-12 16:26:30 +0000

    Show all response info when no filters supplied

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	customKey := flag.String("ck", "", "add a custom header to all requests (key)")
 	customVal := flag.String("cv", "", "add a custom header to all requests (value)")
 	extension := flag.String("e", "", "file extension to add (without dot prefix)")
-	success := flag.String("s", "200, 204, 301, 302, 307", "status codes indicating success, seperated by commas")
+	success := flag.String("s", "", "status codes indicating success, seperated by commas")
 	failure := flag.String("f", "", "status codes indicating failure, seperated by commas")
 	userAgent := flag.String("a", "discover: https://github.com/bruston/discover", "user-agent to use")
 	cookieFile := flag.String("cookies", "", "file containing cookies")
@@ -114,6 +114,9 @@ func main() {
 					}
 					continue
 				}
+				if len(successCodes) == 0 {
+					fmt.Printf("/%s %d %d\n", word, code, size)
+				}
 				for _, v := range successCodes {
 					if v == code {
 						fmt.Printf("/%s %d %d\n", word, code, size)
@@ -127,6 +130,9 @@ func main() {
 }
 
 func cleanCodes(successCodes string) ([]int, error) {
+	if successCodes == "" {
+		return nil, nil
+	}
 	split := strings.Split(successCodes, ",")
 	codes := []int{}
 	for i := range split {
@@ -155,6 +161,7 @@ func doReq(client *http.Client, url, host, word, customKey, customVal, userAgent
 		req.Header.Set("Cookie", cookies)
 	}
 	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("Connection", "close")
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Print(err)
